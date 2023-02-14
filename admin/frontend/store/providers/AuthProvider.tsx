79719679a68c07e4772d880edb4a7a4628bf6acd
hsingyingli <xingyingli855@gmail.com>
2023-02-14 12:16:17 +0800

    finish redirect logic for not auth

--- admin/frontend/store/providers/AuthProvider.tsx
+++ admin/frontend/store/providers/AuthProvider.tsx
@@ -1,5 +1,6 @@
 import { LoadingPage } from "@/components/Loading";
 import { useRefreshToken } from "@/hooks/useRefreshToken";
+import { useRouter } from "next/router";
 import React, { createContext, useEffect, useState } from "react";
 
 
@@ -33,19 +34,44 @@ const AuthProvider: React.FC<Props> = ({ children }) => {
   const [isLoading, setIsLoading] = useState<boolean>(true)
   const [user, setUser] = useState<User | null>(null)
   const refreshToken = useRefreshToken()
+  const router = useRouter()
+  const path = router.asPath
 
   const updateUser = (user: User | null) => {
     setUser(user)
   }
 
   useEffect(() => {
-    const initUser = async () => {
-      await refreshToken()
-      setIsLoading(false)
-    }
-    initUser()
-
+    const handleFinishRedirect = () => setIsLoading(false)
+    router.events.on("routeChangeComplete", handleFinishRedirect)
+    return () => router.events.off("routeChangeComplete", handleFinishRedirect)
   }, [])
+
+
+  useEffect(() => {
+    setIsLoading(true)
+    const initUser = async () => {
+      const { error } = await refreshToken()
+      if (error === null) {
+        setIsLoading(false)
+      } else {
+        router.push("/login")
+      }
+    }
+
+    const isNotRequiredAuth = path.includes("/login") || path.includes("/signup")
+
+    if (user === null && isNotRequiredAuth) {
+      setIsLoading(false)
+    } else if (user !== null && isNotRequiredAuth) {
+      router.push("/")
+    } else if (user === null && !isNotRequiredAuth) {
+      initUser()
+    }
+
+  }, [path])
+
+
 
   return (
     isLoading ?
